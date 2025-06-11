d0f9e979f5c51e3edee222dba318182cb4bccdbf
Josh_Kuei <kuei790802@me.com>
2025-06-11 15:42:33 +0800

    feat(erp-Inventory): Add Inventory, InventoryRepository, ,InventoryService, InventoryServiceTest
    Adds inventory management entities and logic.
    
    This commit introduces the foundational entities, enums, repositories, and services for inventory management. It sets up database models, defines inventory movement types, and implements core logic for receiving purchase orders and updating inventory levels.
    
    - Add entity classes: Inventory, InventoryMovement, PurchaseOrder, warehouse and Users.
    
    - Create enums package and add enums: DocumentType, MovementType, and PurchaseOrderStatus to represent different states and types within the inventory system.
    
    - Add repositories: InventoryMovementRepository, InventoryRepository and UserRepository.
    
    -Add service: InventoryService.
    
    -Add exception:DataConflictException.
    
    -Add unit test: InventoryServiceTest test.

---
+++ backend/src/main/java/com/example/demo/enums/MovementType.java
@@ -0,0 +1,12 @@
+package com.example.demo.enums;
+
+public enum MovementType {
+    IN_PURCHASE, 
+    OUT_SALE, 
+    // ADJUSTMENT_IN, 
+    // ADJUSTMENT_OUT, 
+    // RETURN_IN, 
+    // RETURN_OUT, 
+    // TRANSFER_IN, 
+    // TRANSFER_OUT
+}
