ee5770c4f4fec4ac54a1bfe40d70b1c3430ff3fd
Josh_Kuei <kuei790802@me.com>
2025-06-12 12:44:11 +0800

    refactor(erp-purchase): implement purchase order enum and
    Restore packages and files lost due to incorrect Git operation.
    
    - Re-add missing PurchaseOrderStatus enum
    - Set up a global exception handler

---
+++ backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(
+        ResourceNotFoundException ex){
+
+            Map<String, Object> body = new HashMap<>();
+            body.put("timestamp",LocalDateTime.now());
+            body.put("status", HttpStatus.NOT_FOUND.value());
+            body.put("error", "Resource_Not_Found");
+            body.put("message", ex.getMessage());
+
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+            
+
+        }
+
+}
