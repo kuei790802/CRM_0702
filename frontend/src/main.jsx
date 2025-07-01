a5fc6a2b02e178249fde450127508faecf7fd5ad
Cassidy-7749 <abz666666a@gmail.com>
2025-07-01 14:40:57 +0800

    little fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./Router.jsx";
 
-// if (import.meta.env.MODE === 'development' || import.meta.env.VITE_USE_MOCK === 'true') {
-//   const { worker } = await import('./mocks/browser');
-//   await worker.start({
-//     onUnhandledRequest: 'bypass',
-//   });
-// }
+if (import.meta.env.MODE === 'development' || import.meta.env.VITE_USE_MOCK === 'true') {
+  const { worker } = await import('./mocks/browser');
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
 
 
 
